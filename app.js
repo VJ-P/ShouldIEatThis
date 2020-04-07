4dbeb5e7531819053380e068f333b46c3bb8ab87
Vijay Patel <108.vijayp@gmail.com>
2020-04-06 23:15:07 -0400

    Add description data for each recipe and refactor

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const   bodyParser = require('body-parser'),
         mongoose = require('mongoose'),
         app = express();
 
-mongoose.connect("mongodb://localhost/should_i_eat_this");
+mongoose.connect("mongodb://localhost:27017/should_i_eat_this", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -11,45 +11,49 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 let recipeSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 let Recipe = mongoose.model("Recipe", recipeSchema);
 
 // Recipe.create(
 //     {
-//         name: "Pecan Pie", 
-//         image: "https://bit.ly/2X7DDhm"
+//         name: "Pizza",
+//         image: "https://bit.ly/2weGHgx",
+//         description: "This is a fantastic version of an Italian classic. The feta cheese adds a rich flavor that brings this dish to life. Incredibly easy and incredibly delicious!"
 //     },
 //     (err, recipe) => {
 //         if(err) {
 //             console.log(err);
 //         } else {
-//             console.log("New Recipe");
+//             console.log("Newly Created Recipe");
 //             console.log(recipe);
 //         }
 //     }
 // );
 
-
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
+// INDEX ROUTE
 app.get("/recipes", (req, res) => {
     Recipe.find({}, (err, allRecipes) => {
         if(err) {
             console.log(err);
         } else {
-            res.render("recipes", {recipes: allRecipes});
+            res.render("index", {recipes: allRecipes});
         }
     })
 });
 
+// CREATE ROUTE - Shows a list of all the recipes
 app.post("/recipes", (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
-    let newRecipe = {name: name, image: image};
+    let description = req.body.description;
+    let newRecipe = {name: name, image: image, description: description};
 
     Recipe.create(newRecipe, (err, newlyCreated) => {
         if(err){
@@ -60,8 +64,22 @@ app.post("/recipes", (req, res) => {
     });
 });
 
+// NEW ROUTE - Shwos form to submit new recipe
 app.get("/recipes/new", (req, res) => {
     res.render("new");
+});
+
+// SHOW ROUTE - Shows more info about a recipe
+app.get("/recipes/:id", (req, res) => {
+    // find the recipe with the provided ID
+    Recipe.findById(req.params.id, (err, foundRecipe) => {
+        if(err) {
+            console.log(err);
+        } else {
+            // render show template with that recipe
+            res.render("show", {recipe: foundRecipe});
+        }
+    });
 });
 
 app.listen(3000, () =>{
