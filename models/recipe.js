518389c96076e8ff87bbd2bb78006773fa3e5626
Vijay Patel <108.vijayp@gmail.com>
2020-05-16 13:43:37 -0400

    Update recipe model to include 5 steps

--- models/recipe.js
+++ models/recipe.js
@@ -5,6 +5,11 @@ let recipeSchema = new mongoose.Schema({
     name: String,
     image: String,
     description: String,
+    step1: String,
+    step2: String,
+    step3: String,
+    step4: String,
+    step5: String,
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
